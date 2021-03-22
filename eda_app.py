75e576854eab453388fdacadb18a138a63b48649
hoon1016 <wjsrms76@gmail.com>
2021-03-22 14:34:02 +0900

    Create eda_app.py

---
+++ eda_app.py
@@ -0,0 +1,91 @@
+import streamlit as st 
+import yfinance as yf 
+import matplotlib.pyplot as plt 
+from datetime import datetime
+import pandas as pd
+import numpy as np 
+import requests
+from fbprophet import Prophet
+
+def run_eda_app():
+    
+    
+
+    symbol = st.text_input('심볼 입력 : ')
+    data = yf.Ticker(symbol)
+
+    today = datetime.now().date().isoformat()
+    print(today)
+
+    df = data.history(start='2010-06-01',end=today)
+
+
+    st.dataframe(df)
+
+    st.subheader('종가')
+    if st.button('종가 보기'):
+        st.line_chart(df['Close'])
+        
+    st.subheader('거래량')
+    if st.button('거래량 보기'):
+        st.line_chart(df['Volume'])
+
+    #yfinace 라이브러리만의 정보 
+
+    # data.info
+    # data.calendar
+    # data.major_holders
+    # data.institutional_holders
+    # data.recommendations
+    
+    div_df=data.dividends
+    
+    st.dataframe(div_df.resample('Y').sum())
+
+    new_df = div_df.reset_index()
+    new_df['Year'] = new_df['Date'].dt.year
+
+    st.dataframe(new_df)
+    
+    if st.button('차트보기'):
+    
+        fig = plt.figure()
+        plt.bar(new_df['Year'],new_df['Dividends'])
+        st.pyplot(fig)
+    # 여러 주식 데이터를 한번에 보여주기.
+    
+    favorites = ['msft','INTC','nvda','aapl','amzn']
+
+    f_df = pd.DataFrame()
+    
+    for stock in favorites :
+        f_df[stock] = yf.Ticker(stock).history(start='2010-01-01',end=today)['Close']
+
+    st.dataframe(f_df)
+    
+    corr_columns = f_df.columns[f_df.dtypes != object]
+    corr_list = st.multiselect('차트를 볼 컬럼을 선택하세요',corr_columns)
+    
+    if len(corr_list) != 0 :
+        st.dataframe(f_df[corr_list].corr())
+        st.line_chart(f_df[corr_list])
+
+    #스탁트윗의 API를 호출!!
+    res = requests.get('https://api.stocktwits.com/api/2/streams/symbol/{}.json'.format(symbol))
+
+    #JSON 형식이므로, .json()이용.
+    res_data = res.json()
+
+    #파이썬의 딕셔너리와 리스트의 조합으로 사용가능
+    # st.write(res_data)
+
+    for message in res_data['messages']:
+        
+        col1,col2 =st.beta_columns([1,4])
+
+        with col1 : 
+            st.image(message['user']['avatar_url'])
+        with col2 :
+            st.write('유저 이름 : ' +message ['user']['username'])
+            st.write('트윗 내용 : ' +message ['body'])
+            st.write('올린 시간 : ' +message ['created_at'])
